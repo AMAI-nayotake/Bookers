8c3da8efc3340e264968838733fea0e050e8adc0
AMAI-nayotake <194278002+AMAI>
2025-03-14 17:59:20 +0000

    [Start] indexにtable作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
 
  def index
+  @book = Book.new
   @books = Book.all
  end
 
  def create
-  book = Book.new
+  book = Book.new(book_params)
   if book.save
     redirect_to book_path(book.id)
   else
@@ -25,6 +26,11 @@ class BooksController < ApplicationController
   book = Book.find(params[:id])
   book.update(book_params)
   redirect_to book_path(book.id)
+ end
+
+ private
+ def book_params
+   params.require(:book).permit(:title, :body)
  end
 
 end
