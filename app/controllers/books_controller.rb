99e4ff5822284dff2466e702ec00d7ad55bbf212
AMAI-nayotake <194278002+AMAI>
2025-03-20 13:11:09 +0000

    [Finish] バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,10 +25,10 @@ def edit
 end
 
 def update
-  book = Book.find(params[:id])
-   if book.update(book_params)
+  @book = Book.find(params[:id])
+   if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
    else
     render :edit
    end
